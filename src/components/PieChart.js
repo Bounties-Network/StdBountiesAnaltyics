175d24a6365108cf54c82a136584021c2841b055
Brian Ho <mr.brianhobo@hotmail.com>
2018-07-25 15:05:32 -0700

    fix category pie chart to display an 'Other' category

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,62 +1,99 @@
 
 import React from 'react';
 import Highcharts from 'highcharts';
+import drilldown from 'highcharts/modules/drilldown.js';
 
-function getSeriesData(datas) {
+function getCategoriesSeriesData(datas) {
 	datas = datas[0].data;
-	datas.sort(function(a, b){return a[1] - b[1]})
+	datas.sort(function(a, b){return b[1] - a[1]});
 
 	const series = [];
+	let count = 0;
 	for (let i = 0; i < datas.length; i += 1) {
-	series.push({
-	  name: datas[i][0],
-	  y: datas[i][1]
-	});
+		if (datas[i][1] < 15) {
+			count += datas[i][1];
+		} else {
+			series.push({
+			  name: datas[i][0],
+			  y: datas[i][1]
+			});
+		}
 	}
+	series.push({
+		name: 'Other',
+		y: count,
+		drilldown: 'Other'
+	});
+	return series;
+}
+
+function getCategoriesDrilldownData(datas) {
+	datas = datas[0].data;
+	datas.sort(function(a, b){return b[1] - a[1]});
+
+	const series = [];
+	let count = 0;
+	for (let i = 0; i < datas.length; i += 1) {
+		if (datas[i][1] <= 3) {
+			count += datas[i][1];
+		}
+		else if (datas[i][1] < 15) {
+			series.push([datas[i][0],datas[i][1]]);
+		}
+	}
+	series.push(['Other', count]);
 	return series;
 }
 
 class PieChart extends React.Component {
 	constructor(props) {
     super(props);
+    drilldown(Highcharts);
   }
 
   // When the DOM is ready, create the chart.
   componentDidMount() {
     this.chart = Highcharts.chart(this.props.id, {
-      chart: {
-        type: 'pie'
-      },
-      credits: {
-        enabled: false
-      },
-      title: {
-        text: this.props.title
-      },
-      subtitle: {
-        text: this.props.subtitle
-      },
-      tooltip: {
-	    pointFormat: '{series.name}: <b>{point.y}</b>'
-	  },
-      plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: {
-                enabled: true,
-                format: '<b>{point.name}</b>: {point.percentage:.1f}%',
-                style: {
-                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                }
-            }
-        }
-      },
-      series: [{
-        name: 'Count',
-        colorByPoint: true,
-        data: getSeriesData(this.props.data)
-	    }]
+		chart: {
+			type: 'pie'
+		},
+		credits: {
+			enabled: false
+		},
+		title: {
+			text: this.props.title
+		},
+		subtitle: {
+			text: this.props.subtitle
+		},
+		tooltip: {
+			pointFormat: '{series.name}: <b>{point.y}</b>'
+		},
+		plotOptions: {
+			pie: {
+			    allowPointSelect: true,
+			    cursor: 'pointer',
+			    dataLabels: {
+			        enabled: true,
+			        format: '<b>{point.name}</b>: {point.percentage:.1f}%',
+			        style: {
+			            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+			        }
+			    }
+			}
+		},
+		series: [{
+			name: 'Count',
+			colorByPoint: true,
+			data: getCategoriesSeriesData(this.props.data)
+		}],
+		drilldown: {
+			series: [{
+				name: 'Count',
+				id: 'Other',
+				data: getCategoriesDrilldownData(this.props.data)
+			}]
+		}
     });
     setTimeout(() => {
       this.chart.reflow();
@@ -64,7 +101,7 @@ class PieChart extends React.Component {
   }
   componentWillReceiveProps(nextProps) {
     if (nextProps.data !== this.props.data) {
-      this.chart.update({ series: getSeriesData(nextProps.data) }, true);
+      this.chart.update({ series: getCategoriesSeriesData(nextProps.data) }, true);
     }
   }
   componentWillUnmount() {
