2f136a28592c5890987f581060ba603788d2ede7
William Villanueva <wvillanu89@gmail.com>
2018-10-23 13:41:31 -0500

    Update endpoint - config for prague

--- src/config.js
+++ src/config.js
@@ -2,7 +2,10 @@ const config = {
   platformList: [
     { value: 'all', name: 'All' },
     { value: 'gitcoin', name: 'Gitcoin' },
-    { value: 'bounties-network', name: 'Bounties Network' }
+    { value: 'bounties-network', name: 'Bounties Network' },
+    { value: 'berlin', name: 'Berlin' },
+    { value: 'sf', name: 'San Francisco' },
+    { value: 'prague', name: 'Prague' },
   ],
   earliestDate: '2017-12-20'
 };
